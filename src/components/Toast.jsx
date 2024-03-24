66d5e65aa25cefcb73f1ace75d6a52d0b473ede4
ivannissimrch <ivannissimrch@gmail.com>
2024-03-24 08:51:15 -0400

    Add animation to toast using framer motion

--- src/components/Toast.jsx
+++ src/components/Toast.jsx
@@ -1,5 +1,7 @@
 import PropTypes from "prop-types";
 import { IoCloseCircleOutline } from "react-icons/io5";
+import { motion } from "framer-motion";
+
 export default function Toast({ newToast, deleteToast }) {
   let toastBgColor = "";
   if (newToast.variant === "notice") {
@@ -16,10 +18,16 @@ export default function Toast({ newToast, deleteToast }) {
   }
 
   return (
-    <li className={` h-20 p-4 m-4 flex justify-between ${toastBgColor}`}>
+    <motion.li
+      initial={{ opacity: 0, x: 100 }}
+      animate={{ opacity: 1, x: 0 }}
+      exit={{ opacity: 0, x: -50 }}
+      transition={{ duration: 0.5 }}
+      className={` h-20 p-4 m-4 flex justify-between ${toastBgColor}`}
+    >
       <p className="flex flex-col">{newToast.message}</p>
       <IoCloseCircleOutline size={20} onClick={() => deleteToast(newToast)} />
-    </li>
+    </motion.li>
   );
 }
 
--- src/components/ToastTray.jsx
+++ src/components/ToastTray.jsx
@@ -1,16 +1,19 @@
 import PropTypes from "prop-types";
 import Toast from "./Toast";
+import { AnimatePresence } from "framer-motion";
 
 export default function ToastTray({ toastBatch, deleteToast }) {
   return (
     <ul className="w-1/2 p-5 m-6">
-      {toastBatch.map((newToast) => (
-        <Toast
-          key={newToast.id}
-          newToast={newToast}
-          deleteToast={deleteToast}
-        />
-      ))}
+      <AnimatePresence>
+        {toastBatch.map((newToast) => (
+          <Toast
+            key={newToast.id}
+            newToast={newToast}
+            deleteToast={deleteToast}
+          />
+        ))}
+      </AnimatePresence>
     </ul>
   );
 }
