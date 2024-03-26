ebb6ae12f2316585f33c95091e97262641242a6f
ivannissimrch <ivannissimrch@gmail.com>
2024-03-25 21:23:11 -0400

    Add icons to Toast component

--- src/components/Toast.jsx
+++ src/components/Toast.jsx
@@ -1,20 +1,29 @@
 import PropTypes from "prop-types";
-import { IoCloseCircleOutline } from "react-icons/io5";
+import { IoClose } from "react-icons/io5";
 import { motion } from "framer-motion";
+import { PiWarningCircleBold } from "react-icons/pi";
+import { PiWarningBold } from "react-icons/pi";
+import { PiWarningOctagonBold } from "react-icons/pi";
+import { IoIosCheckmarkCircle } from "react-icons/io";
 
 export default function Toast({ newToast, deleteToast }) {
   let toastBgColor = "";
+  let icon = "";
   if (newToast.variant === "notice") {
     toastBgColor = "bg-blue-200";
+    icon = <PiWarningCircleBold size={30} />;
   }
   if (newToast.variant === "warning") {
     toastBgColor = "bg-yellow-200";
+    icon = <PiWarningBold size={30} />;
   }
   if (newToast.variant === "success") {
     toastBgColor = "bg-lime-200";
+    icon = <IoIosCheckmarkCircle size={30} />;
   }
   if (newToast.variant === "error") {
     toastBgColor = "bg-red-200";
+    icon = <PiWarningOctagonBold size={30} />;
   }
 
   return (
@@ -23,10 +32,13 @@ export default function Toast({ newToast, deleteToast }) {
       animate={{ opacity: 1, x: 0 }}
       exit={{ opacity: 0, x: -50 }}
       transition={{ duration: 0.5 }}
-      className={` min-h-20 p-4 my-4 md:my-4 flex justify-between ${toastBgColor}`}
+      className={` min-h-20 p-4 my-4 md:my-4 flex justify-between items-center rounded-lg ${toastBgColor}`}
     >
-      <p className="flex flex-grow-1 w-full text-start">{newToast.message}</p>
-      <IoCloseCircleOutline size={20} onClick={() => deleteToast(newToast)} />
+      {icon}
+      <p className="flex flex-grow-1 w-full text-start ml-2">
+        {newToast.message}
+      </p>
+      <IoClose size={20} onClick={() => deleteToast(newToast)} />
     </motion.li>
   );
 }
